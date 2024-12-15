5726a6d9fdb9479ffcdef9719b4353e5a45f5e14
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-12-15 21:57:15 +0100

    Add project files.

---
+++ OdciskiPalcow/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OdciskiPalcow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            string kolor = "";
+            if (Zielone.IsChecked == true)
+            {
+                kolor = "zielone";
+            }
+            if (Niebieskie.IsChecked == true)
+            {
+                kolor = "niebieskie";
+            }
+            if (Piwne.IsChecked == true)
+            {
+                kolor = "piwne";
+            }
+            MessageBox.Show($"{Imie.Text} {Nazwisko.Text} kolor oczu {kolor}");
+        }
+
+        private void Numer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Zdjecie.Source = new BitmapImage(new Uri($"{Numer.Text}-zdjecie.jpg", UriKind.RelativeOrAbsolute));
+            Odcisk.Source = new BitmapImage(new Uri($"{Numer.Text}-odcisk.jpg", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
